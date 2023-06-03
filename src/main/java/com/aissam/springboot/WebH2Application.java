554178bb931c6a6fb1f535180ba8064ec3b1a461
bofhtux <aissamtel@gmail.com>
2023-06-03 13:19:36 +0200

    init

---
+++ src/main/java/com/aissam/springboot/WebH2Application.java
@@ -0,0 +1,13 @@
+package com.aissam.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aissam/springboot/WebH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aissam.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
